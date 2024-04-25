294f84a6c73f0cbfd32de6e1c320788f15c6cd08
nquinbla <nquinbla@myuax.com>
2024-04-25 09:13:10 +0200

    [Módulo A] código clase main

---
+++ src/main/java/A_ObjTresDimensiones/Main_A.java
@@ -0,0 +1,12 @@
+package A_ObjTresDimensiones;
+
+public class Main_A {
+    public static void main(String[] args) {
+
+        Punto3D punto1 = new Punto3D(12, 13, 18);
+        Punto3D punto2 = new Punto3D(8, 14, 0);
+
+        System.out.println(punto1);
+        System.out.println(punto2);
+    }
+}
